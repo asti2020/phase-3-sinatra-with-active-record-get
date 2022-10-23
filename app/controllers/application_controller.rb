201663f0f4bee0b26ca3a4e75aa388b3cb061571
Astika Luintel <astild2020@gmail.com>
2022-10-23 12:10:52 -0400

    commit tree 0610d782ad21a76b50b58e72d6ce30b86d89bcac

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,23 @@ class ApplicationController < Sinatra::Base
     { message: "Hello world" }.to_json
   end
 
+  get '/games' do
+    { games: Game.all.order(:title).limit(1) }.to_json
+  end
+  # games = Game.all.order(:title)
+  # games.to_json
+  # end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+
+    # include associated reviews in the JSON response
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
+  end
+
+
 end
